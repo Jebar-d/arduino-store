a2b90c777a2d067c0e5c2578b318bd99ae9791f7
Gebhard <akosibbear38@gmail.com>
2026-03-06 22:23:12 +0800

    fixed sign up error

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import { onAuthChange } from './auth.js'
 import { headerPartial } from './header.js'
+import { supaAuth } from './auth.js'
 
 // Initialize header on auth state change
 onAuthChange(user => headerPartial(user))
 
 // Check auth and verification on every page
-const { data: { session } } = await supa.auth.getSession()
+const { data: { session } } = await supaAuth.auth.getSession()
 
 if (session?.user && !session.user.email_confirmed_at) {
   // User logged in but not verified - force to verification page
